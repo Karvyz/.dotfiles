d02c801c8201faebb769a53b6e71a381c9688ee0
Henri Gros <henrigros1@gmail.com>
2024-02-24 00:33:06 +0100

    Apparently I need thoses

---
+++ system/gnome.nix
@@ -0,0 +1,36 @@
+
+{ config, pkgs, ... }:
+
+{
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+            ]) ++ (with pkgs.gnome; [
+              cheese # webcam tool
+              gnome-music
+              gedit # text editor
+              epiphany # web browser
+              geary # email reader
+              gnome-characters
+              tali # poker game
+              iagno # go game
+              hitori # sudoku game
+              atomix # puzzle game
+              yelp # Help view
+              gnome-contacts
+              gnome-initial-setup
+            ]);
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "ch";
+    xkbVariant = "fr";
+  };
+}
