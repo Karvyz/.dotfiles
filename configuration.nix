fd417febfa22590b8455585d081a8b3a72018713
Henri Gros <henrigros1@gmail.com>
2024-02-24 00:33:55 +0100

    Gnome config

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./system/gnome.nix
     ];
 
   # Bootloader.
@@ -40,19 +41,6 @@
     LC_PAPER = "fr_FR.UTF-8";
     LC_TELEPHONE = "fr_FR.UTF-8";
     LC_TIME = "fr_FR.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "ch";
-    xkbVariant = "fr";
   };
 
   # Configure console keymap
--- system/gnome.nix
+++ system/gnome.nix
@@ -9,24 +9,30 @@
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
     gnome-tour
-            ]) ++ (with pkgs.gnome; [
-              cheese # webcam tool
-              gnome-music
-              gedit # text editor
-              epiphany # web browser
-              geary # email reader
-              gnome-characters
-              tali # poker game
-              iagno # go game
-              hitori # sudoku game
-              atomix # puzzle game
-              yelp # Help view
-              gnome-contacts
-              gnome-initial-setup
-            ]);
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp # Help view
+    gnome-contacts
+    gnome-initial-setup
+  ]);
+
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    blur-my-shell
+    gsconnect
+  ];
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
 
   # Configure keymap in X11
   services.xserver = {
