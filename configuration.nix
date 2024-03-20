d8cad1d036e00ee9eac819a1c91336dbd4aba4ce
Henri Gros <henrigros1@gmail.com>
2024-03-20 22:40:08 +0100

    mv hardware-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./system/hardware-configuration.nix
       inputs.home-manager.nixosModules.default
 
       ./system/gnome.nix
