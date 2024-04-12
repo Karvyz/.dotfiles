89ea9bb14de31f2c21626e8e3dc8d158d9da715d
Henri Gros <henrigros1@gmail.com>
2024-04-12 11:19:50 +0200

    Kinda works but too much for me

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -3,7 +3,45 @@
 {
   programs.nixvim = {
     enable = true;
-    colorschemes.gruvbox.enable = true;
-    plugins.lualine.enable = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+
+    colorschemes.vscode.enable = true;
+    plugins = {
+      lualine.enable = true;
+      telescope.enable = true;
+      oil.enable = true;
+      treesitter = {
+        enable = true;
+        nixGrammars = false;
+        ensureInstalled = [ "lua" "rust" "c" "python" ];
+	parserInstallDir = "~/.config/nvim/treesitter";
+      };
+      luasnip.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          lua-ls.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installRustc = false;
+            installCargo = false;
+          };
+          clangd.enable = true;
+          pylsp.enable = true;
+        };
+      };
+      cmp = {
+        enable = true;
+	# sources = [
+	#   { name = "nvim-lsp"; }
+	#   { name = "path"; }
+	#   { name = "buffer"; }
+	# ];
+      };
+    };
   };
 }
