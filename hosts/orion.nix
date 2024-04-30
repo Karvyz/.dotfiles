23f23f4abf688704c53c8d2e2d6f022b11db3133
Henri Gros <henrigros1@gmail.com>
2024-04-30 14:16:27 +0200

    NVK -> save battery life

--- hosts/orion.nix
+++ hosts/orion.nix
@@ -10,14 +10,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
 	# Hardware
-	nvidia.enable = true;
+	nvidia.enable = false;
 
   # Desktop
   gnome.enable = true;
   hyprland.enable = true;
   desktop.enable = true;
   devtools.enable = true;
-	ollama.enable = true;
+	ollama.enable = false;
 
   # Enables VMs
   virtualization.enable = true;
