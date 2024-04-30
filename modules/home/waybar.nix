be17cd240a884d57defee65eb8f0155ebc67c328
Henri Gros <henrigros1@gmail.com>
2024-04-30 11:34:24 +0200

    Waybar OCD

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -11,7 +11,7 @@ in {
 
       modules-center = [ "hyprland/workspaces" ] ;
       modules-left = [ "hyprland/window" "pulseaudio" "cpu" "memory"];
-      modules-right = [ "network" "battery" "clock" "tray" ];
+      modules-right = [ "tray" "network" "battery" "clock" ];
 
       "hyprland/workspaces" = {
       	format = "{name}";
@@ -26,7 +26,7 @@ in {
       "clock" = {
 				format = '' {:L%H:%M}'';
       	tooltip = true;
-				tooltip-format = "<big>{:%A, %d.%B %Y }</big><tt><small>{calendar}</small></tt>";
+				tooltip-format = "<tt><small>{calendar}</small></tt>";
       };
       "hyprland/window" = {
       	max-length = 25;
@@ -95,12 +95,30 @@ in {
     		font-weight: bold;
       }
 			window#waybar {
-				border-radius: 0px;
 				background: transparent;
       }
-      #workspaces {
+			.module {
+				padding: 0 10px;
 				background: #${palette.base01};
-				margin: 2px;
+				border-radius: 10px;
+				margin: 10px 10px 0 0;
+			}
+
+			#window {
+				margin-left: 10px;
+				color: #${palette.base05};
+      }
+			#pulseaudio {
+				color: #${palette.base0D};
+			}
+			#cpu {
+				color: #${palette.base07};
+			}
+			#memory {
+				color: #${palette.base0F};	
+			}
+
+			#workspaces {
 				padding: 0px 1px;
 				border-radius: 15px;
 				border: 0px;
@@ -113,125 +131,31 @@ in {
 				border-radius: 10px;
 				border: 0px;
 				color: #${palette.base00};
-				background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-				background-size: 300% 300%;
-				animation: gradient_horizontal 15s ease infinite;
+        background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
+        background-size: 300% 300%;
 				opacity: 0.5;
 				transition: ${betterTransition};
-      }
+			}
       #workspaces button.active {
-				padding: 0px 5px;
-				margin: 4px 3px;
-				border-radius: 10px;
-				border: 0px;
-				color: #${palette.base00};
-				background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-				background-size: 300% 300%;
-				animation: gradient_horizontal 15s ease infinite;
-				transition: ${betterTransition};
 				opacity: 1.0;
 				min-width: 40px;
-      }
+			}
       #workspaces button:hover {
-				border-radius: 10px;
-				color: #${palette.base00};
-				background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-				background-size: 300% 300%;
-				animation: gradient_horizontal 15s ease infinite;
 				opacity: 0.8;
-				transition: ${betterTransition};
       }
-      @keyframes gradient_horizontal {
-				0% {
-					background-position: 0% 50%;
-				}
-				50% {
-					background-position: 100% 50%;
-				}
-				100% {
-					background-position: 0% 50%;
-				}
-      }
-      @keyframes swiping {
-        0% {
-					background-position: 0% 200%;
-				}
-				100% {
-					background-position: 200% 200%;
-				}
-      }
-      tooltip {
-				background: #${palette.base00};
-				border: 1px solid #${palette.base0E};
-				border-radius: 10px;
-      }
-      tooltip label {
-				color: #${palette.base07};
-      }
-      #window {
-				margin: 4px;
-				padding: 2px 10px;
-				color: #${palette.base05};
-				background: #${palette.base01};
-				border-radius: 10px;
-      }
-      #memory {
-				color: #${palette.base0F};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #clock {
+
+
+			#network {
+				color: #${palette.base09};
+			}
+			#battery {
+				color: #${palette.base08};
+			}
+			#clock {
 				color: #${palette.base01};
         background: linear-gradient(45deg, #${palette.base0C}, #${palette.base0F}, #${palette.base0B}, #${palette.base08});
         background-size: 300% 300%;
-        animation: gradient_horizontal 15s ease infinite;
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #cpu {
-				color: #${palette.base07};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #disk {
-				color: #${palette.base03};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #battery {
-				color: #${palette.base08};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #network {
-				color: #${palette.base09};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #tray {
-				color: #${palette.base05};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      }
-      #pulseaudio {
-				color: #${palette.base0D};
-				background: #${palette.base01};
-				margin: 4px;
-				padding: 2px 10px;
-				border-radius: 10px;
-      } '';
+			}
+			'';
   };
 }
