edbcecb951dfdb61dd96b5ea24af67e4a5d1c63d
Henri Gros <henrigros1@gmail.com>
2024-05-29 14:37:53 +0200

    Latest Kernel

--- system/essentials.nix
+++ system/essentials.nix
@@ -4,6 +4,8 @@
 	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
 
+	boot.kernelPackages = pkgs.linuxPackages_latest;	
+
 	environment.systemPackages = with pkgs; [
 		# Neovim
 		neovim
