1b68f93686e8fdf8c6b46cd61d55a11988efb82c
Henri Gros <henrigros1@gmail.com>
2024-06-07 12:57:17 +0200

    Remove Hyprland pkg from HM

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -1,130 +1,135 @@
-{
-	wayland.windowManager.hyprland = {
-		enable = true;
+{ config, ... }:
+let
+	palette = config.lib.stylix.colors;
+in {
+  home.file.".config/hypr/hyprland.conf".text = ''
+monitor=,1920x1200@120,auto,1
 
-		settings = {
-			monitor = ",1920x1200@120,auto,1";
-			env = "XCURSOR_SIZE,24";
+env = XCURSOR_SIZE,24
 
-			general = {
-				gaps_in = 5;
-				gaps_out = 10;
-				border_size = 2;
-				layout = "dwindle";
-				allow_tearing = false;
-			};
+input {
+	kb_layout = ch
+	kb_variant = fr
+	kb_model =
+	kb_options =
+	kb_rules =
+	follow_mouse = 1
+	touchpad {
+		natural_scroll = yes 
+	}
+	sensitivity = 0
+}
 
-			decoration = {
-				rounding = 10;
-				blur = {
-					enabled = true;
-					size = 3;
-					passes = 1;
-				};
-				drop_shadow = false;
-			};
+general {
+	gaps_in = 5
+	gaps_out = 10
+	border_size = 2
+	col.active_border = rgba(${palette.base0D}ff)
+	col.inactive_border = rgba(${palette.base00}cc)
+	layout = dwindle
+	allow_tearing = false
+}
 
-			animations = {
-				enabled = true;
-				bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-				animation = [
-					"windows, 1, 7, myBezier"
-					"windowsOut, 1, 7, default, popin 80%"
-					"border, 1, 10, default"
-					"borderangle, 1, 8, default"
-					"fade, 1, 7, default"
-					"workspaces, 1, 6, default"
-				];
-			};
+decoration {
+	rounding = 10
+	blur {
+		enabled = true
+		size = 3
+		passes = 1
+	}
+	drop_shadow = false
+}
 
-			dwindle = {
-				pseudotile = true;
-				preserve_split = true;
-			};
+animations {
+	enabled = yes
+	bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+	animation = windows, 1, 7, myBezier
+	animation = windowsOut, 1, 7, default, popin 80%
+	animation = border, 1, 10, default
+	animation = borderangle, 1, 8, default
+	animation = fade, 1, 7, default
+	animation = workspaces, 1, 6, default
+}
 
-			master.new_is_master = true;
-			gestures.workspace_swipe = true;
-			misc.force_default_wallpaper = 0;
+dwindle {
+	pseudotile = yes
+	preserve_split = yes
+}
 
-			exec-once = [
-				"waybar"
-				"pypr"
-				"nm-applet"
-			];
-			
-			"$mainMod" = "SUPER";
-			bindr = [ "$mainMod, SUPER_L, exec, rofi -show drun" ];
-			bindm = [
-				"$mainMod, mouse:272, movewindow"
-				"$mainMod SHIFT, mouse:272, resizewindow"
-			];
-			bind = [
-				"$mainMod, A, exec, kitty"
-				"$mainMod, C, killactive"
-				"$mainMod, M, exit"
-				"$mainMod, E, exec, nautilus"
-				"$mainMod, F, togglefloating"
-				"$mainMod, P, pseudo"
-				"$mainMod, J, togglesplit"
-				"$mainMod, U, exec, pkill waybar"
-				"$mainMod, I, exec, waybar"
-				"$mainMod, N, exec, hyprlock"
-				"$mainMod, I, exec, pypr change_workspace +1"
-				"$mainMod, U, exec, pypr change_workspace -1"
-				"$mainMod, Q, exec, pypr toggle term"
-				"$mainMod, V, exec, pypr toggle volume"
+master {
+	new_is_master = true
+}
 
-				"$mainMod, H, movefocus, l"
-				"$mainMod, L, movefocus, r"
-				"$mainMod, K, movefocus, u"
-				"$mainMod, J, movefocus, d"
+gestures {
+	workspace_swipe = on 
+}
 
-				"$mainMod, 1, workspace, 1"
-				"$mainMod, 2, workspace, 2"
-				"$mainMod, 3, workspace, 3"
-				"$mainMod, 4, workspace, 4"
-				"$mainMod, 5, workspace, 5"
-				"$mainMod, 6, workspace, 6"
-				"$mainMod, 7, workspace, 7"
-				"$mainMod, 8, workspace, 8"
-				"$mainMod, 9, workspace, 9"
-				"$mainMod, 0, workspace, 10"
+misc {
+	force_default_wallpaper = 0
+}
 
-				"$mainMod SHIFT, 1, movetoworkspace, 1"
-				"$mainMod SHIFT, 2, movetoworkspace, 2"
-				"$mainMod SHIFT, 3, movetoworkspace, 3"
-				"$mainMod SHIFT, 4, movetoworkspace, 4"
-				"$mainMod SHIFT, 5, movetoworkspace, 5"
-				"$mainMod SHIFT, 6, movetoworkspace, 6"
-				"$mainMod SHIFT, 7, movetoworkspace, 7"
-				"$mainMod SHIFT, 8, movetoworkspace, 8"
-				"$mainMod SHIFT, 9, movetoworkspace, 9"
-				"$mainMod SHIFT, 0, movetoworkspace, 10"
-	
-				"$mainMod, mouse_down, workspace, e+1"
-				"$mainMod, mouse_up, workspace, e-1"
+exec-once = waybar
+exec-once = pypr
+exec-once = wp-init 
+exec-once = nm-applet
 
-				", XF86MonBrightnessUp, exec, brightnessctl s +5%"
-				", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
 
-				", xf86KbdBrightnessUp, exec, brightnessctl -d *::kbd_backlight set +33%"
-				", xf86KbdBrightnessDown, exec, brightnessctl -d *::kbd_backlight set 33%-"
-			];
+$mainMod = SUPER
 
-			input = {
-				kb_layout = "ch";
-				kb_variant = "fr";
-				kb_model = "";
-				kb_options = "";
-				kb_rules = "";
-				follow_mouse = 1;
-				touchpad = {
-					natural_scroll = true;
-				};
-				sensitivity = 0;
-			
-			};
+bindr = SUPER, SUPER_L, exec, rofi -show drun
 
-		};
-	};
+bind = $mainMod, A, exec, kitty
+bind = $mainMod, C, killactive, 
+bind = $mainMod, M, exit, 
+bind = $mainMod, E, exec, nautilus
+bind = $mainMod, F, togglefloating, 
+bind = $mainMod, P, pseudo, # dwindle
+bind = $mainMod, J, togglesplit, # dwindle
+bind = $mainMod, U, exec, pkill waybar
+bind = $mainMod, I, exec, waybar
+bind = $mainMod, N, exec, hyprlock
+bind = $mainMod, I, exec, pypr change_workspace +1
+bind = $mainMod, U, exec, pypr change_workspace -1
+bind = $mainMod, Q, exec, pypr toggle term
+bind = $mainMod, V, exec, pypr toggle volume
+
+bind = $mainMod, H, movefocus, l
+bind = $mainMod, L, movefocus, r
+bind = $mainMod, K, movefocus, u
+bind = $mainMod, J, movefocus, d
+
+bind = $mainMod, 1, workspace, 1
+bind = $mainMod, 2, workspace, 2
+bind = $mainMod, 3, workspace, 3
+bind = $mainMod, 4, workspace, 4
+bind = $mainMod, 5, workspace, 5
+bind = $mainMod, 6, workspace, 6
+bind = $mainMod, 7, workspace, 7
+bind = $mainMod, 8, workspace, 8
+bind = $mainMod, 9, workspace, 9
+bind = $mainMod, 0, workspace, 10
+
+bind = $mainMod SHIFT, 1, movetoworkspace, 1
+bind = $mainMod SHIFT, 2, movetoworkspace, 2
+bind = $mainMod SHIFT, 3, movetoworkspace, 3
+bind = $mainMod SHIFT, 4, movetoworkspace, 4
+bind = $mainMod SHIFT, 5, movetoworkspace, 5
+bind = $mainMod SHIFT, 6, movetoworkspace, 6
+bind = $mainMod SHIFT, 7, movetoworkspace, 7
+bind = $mainMod SHIFT, 8, movetoworkspace, 8
+bind = $mainMod SHIFT, 9, movetoworkspace, 9
+bind = $mainMod SHIFT, 0, movetoworkspace, 10
+
+bind = $mainMod, mouse_down, workspace, e+1
+bind = $mainMod, mouse_up, workspace, e-1
+
+bindm = $mainMod, mouse:272, movewindow
+bindm = $mainMod SHIFT, mouse:272, resizewindow
+
+bind = , XF86MonBrightnessUp, exec, brightnessctl s +5%
+bind = , XF86MonBrightnessDown, exec, brightnessctl s 5%-
+
+bind = , xf86KbdBrightnessUp, exec, brightnessctl -d *::kbd_backlight set +33%
+bind = , xf86KbdBrightnessDown, exec, brightnessctl -d *::kbd_backlight set 33%-
+	'';
 }
