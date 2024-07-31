a1d8a98f4f2c2de98c055b288095e6ffc7ebd991
Henri Gros <henrigros1@gmail.com>
2024-07-31 21:32:15 +0200

    Distrobox

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -9,9 +9,27 @@
     environment.systemPackages = with pkgs; [
       virtiofsd
 			gnome.gnome-boxes
+	
+			distrobox
+			dive            # look into docker image layers
+    	podman-tui      # status of containers in the terminal
+    	podman-compose  # start group of containers for dev
     ];
   
-    virtualisation.libvirtd.enable = true;
-    virtualisation.spiceUSBRedirection.enable = true;
+    virtualisation =  {
+			libvirtd.enable = true;
+    	spiceUSBRedirection.enable = true;
+
+			containers.enable = true;
+			podman = {
+				enable = true;
+
+				# Create a `docker` alias for podman, to use it as a drop-in replacement
+				dockerCompat = true;
+
+				# Required for containers under podman-compose to be able to talk to each other.
+				defaultNetwork.settings.dns_enabled = true;
+			};
+		};
   };
 }
