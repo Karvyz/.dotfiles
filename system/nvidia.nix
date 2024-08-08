a2b004be3cb1fa294b59954d966e8064b3ce7e53
Henri Gros <henrigros1@gmail.com>
2024-08-08 15:51:57 +0200

    Nvidia + Podman > Docker

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -6,9 +6,6 @@
   };
 
   config = lib.mkIf config.nvidia.enable {
-	  # Enable graphics driver in NixOS unstable/NixOS 24.11
-		hardware.graphics.enable = true;
-
 		# Load "nvidia" driver for Xorg and Wayland
 		# Or if you have an older card, you may have to use
 		# one of the legacy drivers: 
@@ -16,45 +13,49 @@
 		# Source: https://nixos.org/manual/nixos/stable/#sec-x11-graphics-cards-nvidia
 		services.xserver.videoDrivers = ["nvidia"];
 
-		hardware.nvidia = {
+	  # Enable graphics driver in NixOS unstable/NixOS 24.11
+		hardware = {
+			graphics.enable = true;
 
-			# Modesetting is required.
-			modesetting.enable = true;
+			nvidia = {
+				# Modesetting is required.
+				modesetting.enable = true;
 
-			# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-			# Enable this if you have graphical corruption issues or application crashes after waking
-			# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-			# of just the bare essentials.
-			powerManagement.enable = false;
+				# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+				# Enable this if you have graphical corruption issues or application crashes after waking
+				# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+				# of just the bare essentials.
+				powerManagement.enable = false;
 
-			# Fine-grained power management. Turns off GPU when not in use.
-			# Experimental and only works on modern Nvidia GPUs (Turing or newer).
-			powerManagement.finegrained = true;
+				# Fine-grained power management. Turns off GPU when not in use.
+				# Experimental and only works on modern Nvidia GPUs (Turing or newer).
+				powerManagement.finegrained = true;
 
-			# Use the NVidia open source kernel module (not to be confused with the
-			# independent third-party "nouveau" open source driver).
-			# Support is limited to the Turing and later architectures. Full list of 
-			# supported GPUs is at: 
-			# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-			# Only available from driver 515.43.04+
-			# Currently alpha-quality/buggy, so false is currently the recommended setting.
-			open = false;
+				# Use the NVidia open source kernel module (not to be confused with the
+				# independent third-party "nouveau" open source driver).
+				# Support is limited to the Turing and later architectures. Full list of 
+				# supported GPUs is at: 
+				# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+				# Only available from driver 515.43.04+
+				# Currently alpha-quality/buggy, so false is currently the recommended setting.
+				open = false;
 
-			# Enable the Nvidia settings menu,
-		# accessible via `nvidia-settings`.
-			nvidiaSettings = true;
+				# Enable the Nvidia settings menu,
+			# accessible via `nvidia-settings`.
+				nvidiaSettings = true;
 
-			# Optionally, you may need to select the appropriate driver version for your specific GPU.
-			package = config.boot.kernelPackages.nvidiaPackages.stable;
+				# Optionally, you may need to select the appropriate driver version for your specific GPU.
+				package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-			prime = {
-				offload = {
-					enable = true;
-					enableOffloadCmd = true;
+				prime = {
+					offload = {
+						enable = true;
+						enableOffloadCmd = true;
+					};
+					# Make sure to use the correct Bus ID values for your system!
+					amdgpuBusId = "PCI:8:0:0";
+					nvidiaBusId = "PCI:1:0:0";
 				};
-				# Make sure to use the correct Bus ID values for your system!
-				amdgpuBusId = "PCI:8:0:0";
-				nvidiaBusId = "PCI:1:0:0";
 			};
 		};
 	};
--- system/virtualization.nix
+++ system/virtualization.nix
@@ -9,24 +9,15 @@
     environment.systemPackages = with pkgs; [
       virtiofsd
 			gnome.gnome-boxes
-	
+
 			distrobox
-			dive            # look into docker image layers
-    	docker-compose  # start group of containers for dev
-			nvidia-container-toolkit
     ];
-  
+ 		hardware.nvidia-container-toolkit.enable = true; 
     virtualisation =  {
 			libvirtd.enable = true;
     	spiceUSBRedirection.enable = true;
-
-			containers.enable = true;
-			docker = {
+			podman = {
 				enable = true;
-				rootless = {
-  				enable = true;
-					setSocketVariable = true;
-				};
 			};
 		};
   };
