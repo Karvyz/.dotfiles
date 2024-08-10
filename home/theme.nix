fe4d4bccd8912de59f1a11657ac4f27df786442f
Henri Gros <henrigros1@gmail.com>
2024-08-11 00:05:11 +0200

    Wallpaper from URL

--- home/theme.nix
+++ home/theme.nix
@@ -2,7 +2,10 @@
 {
   stylix = {
 		enable = true;
-    image = ./wallpapers/BH.png;
+    image = pkgs.fetchurl {
+			url = "https://w.wallhaven.cc/full/g8/wallhaven-g83qpe.jpg";
+			sha256 = "sha256-orMabHyGVacR9m/sK9Am0G1+tYopvwa/n0K20kPNI9A=";
+  	};
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ashes.yaml";
 		polarity = "dark";
 		opacity.terminal = 0.85;
