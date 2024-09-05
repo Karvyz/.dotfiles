e50341e2c136029f217b8aeb2fce11aca5bb75b7
Henri Gros <henrigros1@gmail.com>
2024-09-05 17:50:37 +0200

    Seti Theme

--- home/theme.nix
+++ home/theme.nix
@@ -6,7 +6,7 @@
 			url = "https://w.wallhaven.cc/full/g8/wallhaven-g83qpe.jpg";
 			sha256 = "sha256-orMabHyGVacR9m/sK9Am0G1+tYopvwa/n0K20kPNI9A=";
   	};
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ashes.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/seti.yaml";
 		polarity = "dark";
 		opacity.terminal = 0.85;
 		cursor = {
