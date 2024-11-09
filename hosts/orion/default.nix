8f5e68527ccfb2a34a231a1c9b2d628954d9b639
Henri Gros <henrigros1@gmail.com>
2024-11-09 19:05:38 +0100

    Remove Nvidia + Steam on Orion

--- hosts/orion/default.nix
+++ hosts/orion/default.nix
@@ -7,14 +7,6 @@
   gnome.enable = true;
   hyprland.enable = true;
   desktop.enable = true;
-	ollama.enable = false;
-
-	specialisation = {
-		NVIDIA.configuration = {
-			nvidia.enable = true;
-			games.enable = true;
-		};
-	};
 
   # Enables VMs
   virtualization.enable = true;
