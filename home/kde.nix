6d78786f89be2529ce1289ff156a763ebe185abc
Henri Gros <henrigros1@gmail.com>
2025-01-26 11:08:46 +0100

    Plasma home improvements

--- home/kde.nix
+++ home/kde.nix
@@ -17,19 +17,35 @@
       }
     ];
 
-    hotkeys.commands = {
-      "launch-kitty" = {
-        name = "Launch Kitty";
-        key = "Meta+A";
-        command = "kitty";
+    shortcuts = {
+      kwin = {
+        # "Overview" = "Meta";
+        "Overview" = "Meta,Meta+W,Toggle Overview";
+        # "Window Close" = "Meta+C";
+        "Window Close" = "Meta+C,Alt+F4,Close Window";
+        # "services/kitty.desktop"."_launch" = "Meta+A";
       };
     };
 
-    shortcuts = {
-      kwin = {
-        "Overview" = "Meta";
-        "Window Close" = "Meta+C";
-      };
+    configFile = {
+      "ksmserverrc"."General"."loginMode" = "emptySession";
+      "kwinrc"."Effect-kwin4_effect_geometry_change"."Duration" = 500;
+      "kwinrc"."MaximizeTile"."DisableOutlineTile" = false;
+      "kwinrc"."MaximizeTile"."DisableRoundTile" = false;
+      "kwinrc"."Plugins"."kinetic_scaleEnabled" = true;
+      "kwinrc"."Plugins"."kinetic_squashEnabled" = true;
+      "kwinrc"."Plugins"."kwin4_effect_geometry_changeEnabled" = true;
+      "kwinrc"."Plugins"."poloniumEnabled" = true;
+      "kwinrc"."Plugins"."scaleEnabled" = false;
+      "kwinrc"."Plugins"."squashEnabled" = false;
+      "kwinrc"."PrimaryOutline"."ActiveOutlineUseCustom" = false;
+      "kwinrc"."PrimaryOutline"."ActiveOutlineUsePalette" = true;
+      "kwinrc"."PrimaryOutline"."InactiveOutlineThickness" = 2;
+      "kwinrc"."PrimaryOutline"."InactiveOutlineUseCustom" = false;
+      "kwinrc"."PrimaryOutline"."InactiveOutlineUsePalette" = true;
+      "kwinrc"."PrimaryOutline"."OutlineThickness" = 2;
+      "kwinrc"."Windows"."DelayFocusInterval" = 0;
+      "kwinrc"."Windows"."FocusPolicy" = "FocusStrictlyUnderMouse";
     };
   };
 }
