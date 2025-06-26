9dc96019b9ee06bcb04c2db2547e1f9788b1bc88
Henri Gros <henrigros1@gmail.com>
2025-06-26 09:22:15 +0200

    Orion Wireguard

--- hosts/orion/software-specific/default.nix
+++ hosts/orion/software-specific/default.nix
@@ -1,5 +1,8 @@
 { inputs, ... }:
 {
+  networking.wg-quick.interfaces.wg0.configFile =
+    "/home/karviz/Documents/config_wireguard_wiretest.conf";
+
   home-manager = {
     backupFileExtension = "hm-backup";
     extraSpecialArgs = {
