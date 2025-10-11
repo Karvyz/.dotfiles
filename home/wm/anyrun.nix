bbb677132ec77aa1342915438b2d38cdcd2af37b
Henri Gros <henrigros1@gmail.com>
2025-10-12 00:28:32 +0200

    Anyrun improvements

--- home/wm/anyrun.nix
+++ home/wm/anyrun.nix
@@ -30,24 +30,72 @@ in
     '';
 
     extraCss = ''
-      --
       @define-color window-background-color ${palette.base00};
       @define-color entry-background-color ${palette.base01};
-      @define-color border-color ${palette.base05};
+      @define-color border-color ${palette.base0D};
 
       window {
-        background-color: @window-background-color;
-        border-radius: 12px;
-        border: 2px solid @border-color;
-        margin: 12px;
+        background: transparent;
       }
 
-      entry {
-        background-color: @entry-background-color;
+      box.main {
+        padding: 5px;
+        border-radius: 10px;
         border: 2px solid @border-color;
-        box-shadow: none;
-        padding: 0px 8px;
+        background-color: @window-background-color;
+        box-shadow: 0 0 5px black;
+      }
+
+      box.main.text {
+        color: #FF0000;
+        background-color: #00FF00;
+        border-color: #0000FF;
+      }
+
+      text {
+        min-height: 30px;
+        padding: 5px;
+        border-radius: 5px;
+      }
+
+      .matches {
+        border-radius: 10px;
+      }
+
+      box.plugin:first-child {
+        margin-top: 5px;
+      }
+
+      box.plugin.info {
+        min-width: 200px;
+      }
+
+      label.match.description {
+        font-size: 10px;
+      }
+
+      label.plugin.info {
         font-size: 14px;
+      }
+
+      .match {
+        background: transparent;
+      }
+
+      .match:selected {
+        border-left: 4px solid @border-color;
+        background: transparent;
+        animation: fade 0.1s linear;
+      }
+
+      @keyframes fade {
+        0% {
+          opacity: 0;
+        }
+
+        100% {
+          opacity: 1;
+        }
       }
     '';
   };
