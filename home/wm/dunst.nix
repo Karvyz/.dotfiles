47c2f1763f3fcb9879053ef437d8f26300c3c787
Henri Gros <henrigros1@gmail.com>
2025-11-02 00:29:05 +0100

    dunst border option update

--- home/wm/dunst.nix
+++ home/wm/dunst.nix
@@ -1,6 +1,11 @@
+{ config, ... }:
 {
   services.dunst = {
     enable = true;
-    settings.global.corner_radius = 10;
+    settings.global = {
+      corner_radius = config.stylix.borders.radius;
+      frame_width = config.stylix.borders.width;
+      offset = "(10,10)";
+    };
   };
 }
