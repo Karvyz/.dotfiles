cdd09620a833cbc23205e456a0deb3bb6b879312
Henri Gros <henrigros1@gmail.com>
2025-11-08 15:08:20 +0100

    Zellij Kill on quit

--- home/shell/zellij.nix
+++ home/shell/zellij.nix
@@ -5,6 +5,7 @@
       pane_frames = false;
       show_startup_tips = false;
       show_release_notes = false;
+      on_force_close = "quit";
       ui.pane_frames.hide_session_name = true;
     };
     enableZshIntegration = true;
