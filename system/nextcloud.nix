e73d84b97c6f3ddf868c76337c19f21569af65d5
Henri Gros <henrigros1@gmail.com>
2025-12-12 23:58:34 +0100

    Nextcloud 32

--- system/nextcloud.nix
+++ system/nextcloud.nix
@@ -12,7 +12,7 @@
   config = lib.mkIf config.nextcloud.enable {
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud31;
+      package = pkgs.nextcloud32;
       extraAppsEnable = true;
       extraApps = {
         inherit (config.services.nextcloud.package.packages.apps)
