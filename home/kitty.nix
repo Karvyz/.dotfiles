b6d4351e04406ffcefafddb57825988851cab317
Henri Gros <henrigros1@gmail.com>
2025-12-16 23:26:09 +0100

    Kitty change font size binds

--- home/kitty.nix
+++ home/kitty.nix
@@ -6,5 +6,10 @@
       wheel_scroll_min_lines = 1;
       confirm_os_window_close = 0;
     };
+
+    keybindings = {
+      "ctrl+shift+j" = "change_font_size all -2.0";
+      "ctrl+shift+k" = "change_font_size all +2.0";
+    };
   };
 }
