c1d85db9f019b20967333cacefe5f6ba1f0bcf99
Henri Gros <henrigros1@gmail.com>
2026-02-17 22:07:41 +0100

    Minimal zellij

--- home/shell/zellij.nix
+++ home/shell/zellij.nix
@@ -8,7 +8,26 @@
       on_force_close = "quit";
       ui.pane_frames.hide_session_name = true;
     };
+    extraConfig = ''
+      keybinds {
+        shared_except "locked" {
+          bind "Ctrl y" {
+            LaunchOrFocusPlugin "file:~/.config/zellij/plugins/room.wasm" {
+              floating true
+              ignore_case true
+              quick_jump true
+            }
+          }
+        }
+      }
+    '';
     enableZshIntegration = true;
     exitShellOnExit = true;
   };
+
+  home.file.".config/zellij/layouts/default.kdl".text = ''
+    layout {
+      pane
+    }
+  '';
 }
